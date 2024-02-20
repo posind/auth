1771c641d38411fa056d294d05f22fca9934f39b
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-20 22:20:26 +0700

    kosong

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.
 import {getWithHeader} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
+if (getCookie("login")===""){
+    redirect("/");
+}
 
 getWithHeader("https://mrt.ulbi.ac.id/notif/ux/getdatauser","login",getCookie("login"),responseFunction)
 
