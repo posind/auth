67b0de44f3ddd4615ffe99d356329e0d173915b7
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-20 20:56:01 +0700

    ke report

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import {getWithHeader} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
+import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
 
-let log=getCookie("login");
-setInner("content",log);
-getWithHeader("https://mrt.ulbi.ac.id/notif/ux/getdatauser","login",log,responseFunction)
+getWithHeader("https://mrt.ulbi.ac.id/notif/ux/getdatauser","login",getCookie("login"),responseFunction)
 
 function responseFunction(result){
-    setInner("content",result);
+    setInner("content","Selamat Datang "+result.nama);
+    redirect("report");
     console.log(result);
 }
