cc78047d8fea6abe5bda99e219d1bf786c1b09f4
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-20 20:49:58 +0700

    cuba test mrt config

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import {getWithHeader} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 
 
 let log=getCookie("login");
-
 setInner("content",log);
+getWithHeader("https://mrt.ulbi.ac.id/notif/ux/getdatauser","login",log,responseFunction)
+
+function responseFunction(result){
+    setInner("content",result);
+}
