88c6d8c9a4450a7c00fdc4e148b2f7cdb9d4d166
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-20 20:24:25 +0700

    firest

---
+++ index.js
@@ -0,0 +1,7 @@
+import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
+import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+
+
+let log=getCookie("login");
+
+setInner("content",log);
