d24453eafc8f73b5eb238ec4a3f9c9df2e3878de
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-29 15:03:11 +0700

    kasih pagar

--- whatsauth.js
+++ whatsauth.js
@@ -5,6 +5,7 @@ import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.3.3/confi
 //definisikan url wss dan keyword menggunakan base64
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
 wauthparam.keyword="aHR0cHM6Ly93YS5tZS82MjgzMTMxODk1MDAwP3RleHQ9d2g0dDVhdXRoMA==";
+wauthparam.redirect="#";
 
 //delete cookies session and call whatsauth qrController
 //deleteCookie(wauthparam.tokencookiename);
