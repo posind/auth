8b96e3a7c8a29c7a542a6cfebce151465c4e2386
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-23 15:54:01 +0700

    coba test http status

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
-import {getWithHeader} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
+import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
 import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
 if (getCookie("login")===""){
     redirect("/");
 }
 
-getWithHeader("https://api.do.my.id/data/user","login",getCookie("login"),responseFunction)
+getJSON("https://api.do.my.id/data/user","login",getCookie("login"),responseFunction)
 
 function responseFunction(result){
-    if (result.phonenumber===""){
-        var pesan="Silahkan lakukan pendaftaran terlebih dahulu";
+    if (result.status === 404){
+        setInner("content","Silahkan lakukan pendaftaran terlebih dahulu "+result.data.nama);
+        //redirect("/signup");
     }else{
-        pesan="Selamat datang "+result.nama;
+        setInner("content","Selamat datang "+result.data.nama);
     }
-    setInner("content",pesan);
-    //redirect("/report");
     console.log(result);
 }
