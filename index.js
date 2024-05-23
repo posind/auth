5122ceef9800a22a8f6827cdee563f19c9c265c1
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-23 16:07:35 +0700

    salah variabel

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ getJSON("https://api.do.my.id/data/user","login",getCookie("login"),responseFunc
 
 function responseFunction(result){
     if (result.status === 404){
-        setInner("content","Silahkan lakukan pendaftaran terlebih dahulu "+result.data.nama);
+        setInner("content","Silahkan lakukan pendaftaran terlebih dahulu "+result.data.name);
         //redirect("/signup");
     }else{
         setInner("content","Selamat datang "+result.data.nama);
