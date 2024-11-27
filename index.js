182ee89822230d28a5177841729563372d9afd11
Dwi Puspa Firdaus <147034299+DwiFirdaus08@users.noreply.github.com>
2024-11-27 12:05:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ getJSON("https://asia-southeast2-civil-epigram-429004-t8.cloudfunctions.net/piba
 function responseFunction(result){
     if (result.status === 200){
         setInner("content","Selamat datang "+result.data.name);
-        redirect("/dashboard-admin");
+        redirect("/dashboard");
     }else{
         setInner("content","Silahkan lakukan chat ke bot helpdesk pemilihan operator");
         redirect("https://wa.me/62895800006000?text=bantuan+operator");
